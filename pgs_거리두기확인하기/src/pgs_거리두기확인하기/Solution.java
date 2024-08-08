d846ba9642c20465ae8df285952f88f9bcf0ad51
pasongvan <pasongvaan@gmail.com>
2024-08-08 21:37:00 +0900

    pgs 거리두기 lv2 1h31m

--- pgs_거리두기확인하기/src/pgs_거리두기확인하기/Solution.java
+++ pgs_거리두기확인하기/src/pgs_거리두기확인하기/Solution.java
@@ -28,7 +28,7 @@ class Solution {
                     String seat = place[row].substring(col, col+1);
                     
                     // 사람이 있고, 방문하지 않았다면 dfs 탐색
-                    if (seat=="P" && !visit[row][col]) {
+                    if (seat.equals("P") && !visit[row][col]) {
                         result = dfs(row, col, 0);
                         // 결과로 0이 나왔으면 break
                         if (result == 0)
@@ -44,25 +44,32 @@ class Solution {
     
     // dfs 메소드
     public static int dfs(int row, int col, int distance) {
+    	// visit 저장
     	visit[row][col] = true;
-    	
-    	// 거리 안에 사람이 있으면 0 리턴
-    	if (distance!=0 && place[row].substring(col, col+1).equals("P"))
-        	return 0;
-    	
+    	// distance가 0보다 크고 P이면 0 리턴
+    	if (distance>0 && place[row].substring(col, col+1).equals("P"))
+    		return 0;
+    	// distance 증가
     	distance++;
-    	
-        // 네 방향 탐색
-    	for (int didx=0; didx<4; didx++) {
-            int newRow = row + dr[didx];
-            int newCol = col + dr[didx];
-            
-            // distance 2 미만 & 인덱스 범위 내 & 빈 테이블이면
-            if (distance<2 && newRow>=0 && newRow<5 && newCol>=0 && newCol<5 && place[newRow].substring(newCol, newCol+1).equals("O")) {
-            	dfs(newRow, newCol, distance);
-            }
-        	return 1;    		
+    	// 네방향 반복
+    	for (int i=0; i<4; i++) {
+    		// 인덱스 변경
+    		int newRow = row + dr[i];
+    		int newCol = col + dc[i];
+    		// 거리가 2를 초과하지 않고, 인덱스도 안 벗어나고
+            // 방문 안했고, 파티션이 아니면 하나 더 탐색
+    		if (distance <= 2 &&
+    				newRow >= 0 && newRow<5 && newCol >= 0 && newCol <5 &&
+                    !visit[newRow][newCol] &&
+    				!place[newRow].substring(newCol, newCol+1).equals("X")) {
+    			int result = dfs(newRow, newCol, distance);
+    			// 리턴 값이 0이면 0리턴
+    			if (result == 0)
+    				return 0;
+    		}
     	}
+    	// 문제 없다면 1 리턴
+    	return 1;
     }
 }
 
